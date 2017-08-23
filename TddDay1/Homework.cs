99b27218dfe7a85a70e84b1215a085c99c463d71
Chen Yongxiu <sheraium@gmail.com>
2017-08-23 22:44:07 +0800

    Complete production code

--- TddDay1/Homework.cs
+++ TddDay1/Homework.cs
@@ -8,25 +8,53 @@ namespace TddDay1
 {
     public class Homework<T>
     {
-        private IEnumerable<T> data;
+        private List<T> data;
 
         public Homework(IEnumerable<T> data)
         {
-            this.data = data;
+            this.data = data.ToList();
         }
 
         public IEnumerable<int> SelectSum(int Number, string Column)
         {
+            if(Number < 0 || Number > this.data.Count())
+                throw new ArgumentException("SelectNumber Out of range!!");
+
             if (Number == 0)
                 return new [] { 0 };
 
-            if (Number == 3)
-                return new [] { 6, 15, 24, 21 };
+            var dataType = typeof(T);
+            var properties = dataType.GetProperties();
+            var columnProperty = properties.ToList().Find((p) => p.Name == Column);
+            if (columnProperty == null)
+                throw new ArgumentException("SelectNumber Out of range!!");
+            
+            List<int> sumList= new List<int>();
+            for (int i = 0; i < this.data.Count; i+=Number)
+            {
+                int sum = 0;
+                for (int j = 0; j < Number; j++)
+                {
+                    int index = i + j;
+                    if (index < this.data.Count)
+                    {
+                        try
+                        {
+                            int value = Convert.ToInt32(dataType.GetProperty(Column).GetValue(this.data[index], null));
+                            sum += value;
+                        }
+                        catch (Exception e)
+                        {
+                            //Console.WriteLine(e);
+                            //throw;
+                        }
+                    }
+                    else break;
+                }
+                sumList.Add(sum);
+            }
 
-            if (Number == 4)
-                return new [] { 50, 66, 60 };
-
-            throw new ArgumentException("SelectNumber Out of range!!");
+            return sumList;
         }
     }
 }

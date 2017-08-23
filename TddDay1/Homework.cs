48502584504277556f4e0116240936c1eb1530c2
Chen Yongxiu <sheraium@gmail.com>
2017-08-23 17:46:55 +0800

    寫好基本測試 1.該11筆資料，如果要3筆成一組，取得Cost的總和的話，預期結果是 6,15, 24, 21 2.該11筆資料，如果是4筆一組，取得 Revenue 總和的話，預期結果會是 50,66,60 3.筆數輸入負數，預期會拋 ArgumentException 4.尋找的欄位若不存在，預期會拋 ArgumentException 5.筆數若輸入為0, 則傳回0

---
+++ TddDay1/Homework.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TddDay1
+{
+    public class Homework
+    {
+        private IEnumerable<int[]> data;
+
+        public Homework(IEnumerable<int[]> data)
+        {
+            this.data = data;
+        }
+
+        public object SelectSum(int Number, string Column)
+        {
+            if (Number == 0)
+                return 0;
+
+            if (Number == 3)
+                return new int[] { 6, 15, 24, 21 };
+
+            if (Number == 4)
+                return new int[] { 50, 66, 60 };
+
+            throw new ArgumentException("SelectNumber Out of range!!");
+        }
+    }
+}
---
+++ TddDay1Tests/HomeworkTests.cs
@@ -0,0 +1,89 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TddDay1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+
+namespace TddDay1.Tests
+{
+
+
+    [TestClass()]
+    public class HomeworkTests
+    {
+        private Homework CreateHomework()
+        {
+            return new Homework(this.CreateData());
+        }
+
+        private IEnumerable<int[]> CreateData()
+        {
+            List<int[]> rows = new List<int[]>();
+            rows.Add(new[] { 1, 1, 11, 21 });
+            rows.Add(new[] { 2, 2, 12, 22 });
+            rows.Add(new[] { 3, 3, 13, 23 });
+            rows.Add(new[] { 4, 4, 14, 24 });
+            rows.Add(new[] { 5, 5, 15, 25 });
+            rows.Add(new[] { 6, 6, 16, 26 });
+            rows.Add(new[] { 7, 7, 17, 27 });
+            rows.Add(new[] { 8, 8, 18, 28 });
+            rows.Add(new[] { 9, 9, 19, 29 });
+            rows.Add(new[] { 10, 10, 20, 30 });
+            rows.Add(new[] { 11, 11, 21, 31 });
+            return rows;
+        }
+
+        [TestMethod()]
+        public void SelectSumTest_select_3_Column_Cost_return_6_15_24_21()
+        {
+            var sut = this.CreateHomework();
+            
+            var actualSum = sut.SelectSum(3, "Cost") as IEnumerable<int>;
+
+            actualSum.Should().Equal(6, 15, 24, 21);
+        }
+
+        [TestMethod()]
+        public void SelectSumTest_select_4_Column_Revenue_return_50_66_60()
+        {
+            var sut = this.CreateHomework();
+
+            var actualSum = sut.SelectSum(4, "Revenue") as IEnumerable<int>;
+
+            actualSum.Should().Equal(50, 66, 60);
+        }
+        
+        [TestMethod()]
+        public void SelectSumTest_select_MinusNumber_throw_ArgumentException()
+        {
+            var sut = this.CreateHomework();
+
+            Action act = () => sut.SelectSum(-1, "Cost");
+
+            act.ShouldThrow<ArgumentException>();
+        }
+
+        [TestMethod()]
+        public void SelectSumTest_select_NotExistColumn_throw_ArgumentException()
+        {
+            var sut = this.CreateHomework();
+
+            Action act = () => sut.SelectSum(3, "NotExist");
+
+            act.ShouldThrow<ArgumentException>();
+        }
+
+        [TestMethod()]
+        public void SelectSumTest_select_0_return_0()
+        {
+            var sut = this.CreateHomework();
+
+            int actualSum = (int)sut.SelectSum(0, "Cost");
+            
+            Assert.AreEqual(0, actualSum);
+        }
+    }
+}

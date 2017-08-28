63cea2d86aadcc55eac8598dce4e9335591472ff
Chen Yongxiu <sheraium@gmail.com>
2017-08-28 11:05:29 +0800

    Add V2 version

---
+++ TddDay1/DataAnalyzerV2.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TddDay1
+{
+    public class DataAnalyzerV2<T>
+    {
+        private List<T> sourceData;
+
+        public DataAnalyzerV2(IEnumerable<T> SourceData)
+        {
+            this.sourceData = SourceData.ToList();
+        }
+
+        public IEnumerable<int> SelectSum(int GroupBy, string Column)
+        {
+            this.ValidateGroupRange(GroupBy);
+
+            this.ValidateColumn(Column);
+
+            if (GroupBy == 0) return new[] { 0 };
+
+            List<int> sumList= new List<int>();
+            for (int groupIndex = 0; groupIndex < this.sourceData.Count; groupIndex += GroupBy)
+            {
+                int sum = 0;
+                for (int itemIndex = 0; itemIndex < GroupBy; itemIndex++)
+                {
+                    int index = groupIndex + itemIndex;
+                    if (index < this.sourceData.Count)
+                    {
+                        try
+                        {
+                            int value = Convert.ToInt32(typeof(T).GetProperty(Column).GetValue(this.sourceData[index], null));
+                            sum += value;
+                        }
+                        catch (Exception e)
+                        {
+                            //Console.WriteLine(e);
+                            //throw;
+                        }
+                    }
+                    else break;
+                }
+                sumList.Add(sum);
+            }
+
+            return sumList;
+        }
+
+        private void ValidateGroupRange(int Number)
+        {
+            if (Number < 0 || Number > this.sourceData.Count()) throw new ArgumentException("SelectNumber Out of range!!");
+        }
+
+        private void ValidateColumn(string Column)
+        {
+            var properties = typeof(T).GetProperties();
+            var columnProperty = properties.ToList().Find((p) => p.Name == Column);
+            if (columnProperty == null)
+                throw new ArgumentException("SelectNumber Out of range!!");
+        }
+
+        int groupby = 0;
+        public DataAnalyzerV2<T> Groupby(int Number)
+        {
+            this.groupby = Number;
+            return this;
+        }
+
+        public IEnumerable<int> GetSum(string Column)
+        {
+            this.ValidateGroupRange(groupby);
+
+            this.ValidateColumn(Column);
+
+            if (groupby == 0) return new[] { 0 };
+
+            List<int> sumList = new List<int>();
+            for (int groupIndex = 0; groupIndex < this.sourceData.Count; groupIndex += groupby)
+            {
+                int sum = 0;
+                for (int itemIndex = 0; itemIndex < groupby; itemIndex++)
+                {
+                    int index = groupIndex + itemIndex;
+                    if (index < this.sourceData.Count)
+                    {
+                        try
+                        {
+                            int value = Convert.ToInt32(typeof(T).GetProperty(Column).GetValue(this.sourceData[index], null));
+                            sum += value;
+                        }
+                        catch (Exception e)
+                        {
+                            //Console.WriteLine(e);
+                            //throw;
+                        }
+                    }
+                    else break;
+                }
+                sumList.Add(sum);
+            }
+
+            return sumList;
+        }
+    }
+}
---
+++ TddDay1Tests/DataAnalyzerTestsV2.cs
@@ -0,0 +1,88 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TddDay1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+
+namespace TddDay1.Tests
+{
+    [TestClass()]
+    public class DataAnalyzerTestsV2
+    {
+        private DataAnalyzerV2<Product> CreateSUT()
+        {
+            return new DataAnalyzerV2<Product>(this.CreateTestData());
+        }
+
+        private IEnumerable<Product> CreateTestData()
+        {
+            return new List<Product>()
+                       {
+                           new Product() { Id = 1, Cost = 1, Revenue = 11, SellPrice = 21 },
+                           new Product() { Id = 2, Cost = 2, Revenue = 12, SellPrice = 22 },
+                           new Product() { Id = 3, Cost = 3, Revenue = 13, SellPrice = 23 },
+                           new Product() { Id = 4, Cost = 4, Revenue = 14, SellPrice = 24 },
+                           new Product() { Id = 5, Cost = 5, Revenue = 15, SellPrice = 25 },
+                           new Product() { Id = 6, Cost = 6, Revenue = 16, SellPrice = 26 },
+                           new Product() { Id = 7, Cost = 7, Revenue = 17, SellPrice = 27 },
+                           new Product() { Id = 8, Cost = 8, Revenue = 18, SellPrice = 28 },
+                           new Product() { Id = 9, Cost = 9, Revenue = 19, SellPrice = 29 },
+                           new Product() { Id = 10, Cost = 10, Revenue = 20, SellPrice = 30 },
+                           new Product() { Id = 11, Cost = 11, Revenue = 21, SellPrice = 31 }
+                       };
+        }
+
+        [TestMethod()]
+        public void SelectSumTest_select_3_Column_Cost_return_6_15_24_21()
+        {
+            var sut = this.CreateSUT();
+            
+            var actualSum = sut.Groupby(3).GetSum("Cost");
+
+            actualSum.Should().Equal(6, 15, 24, 21);
+        }
+
+        [TestMethod()]
+        public void SelectSumTest_select_4_Column_Revenue_return_50_66_60()
+        {
+            var sut = this.CreateSUT();
+
+            var actualSum = sut.Groupby(4).GetSum("Revenue");
+
+            actualSum.Should().Equal(50, 66, 60);
+        }
+        
+        [TestMethod()]
+        public void SelectSumTest_select_MinusNumber_throw_ArgumentException()
+        {
+            var sut = this.CreateSUT();
+
+            Action act = () => sut.Groupby(-1).GetSum("Cost");
+
+            act.ShouldThrow<ArgumentException>();
+        }
+
+        [TestMethod()]
+        public void SelectSumTest_select_NotExistColumn_throw_ArgumentException()
+        {
+            var sut = this.CreateSUT();
+
+            Action act = () => sut.Groupby(3).GetSum("NotExist");
+
+            act.ShouldThrow<ArgumentException>();
+        }
+
+        [TestMethod()]
+        public void SelectSumTest_select_0_return_0()
+        {
+            var sut = this.CreateSUT();
+
+            var actualSum = sut.Groupby(0).GetSum("Cost");
+
+            actualSum.Should().Equal(0);
+        }
+    }
+}
